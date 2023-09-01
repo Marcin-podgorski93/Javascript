11b8feeddddbad7843f55fc75fb6aa41bb0adcf1
marcinpodgorski <podgorski.marcin93@gmail.com>
2023-09-01 16:41:56 +0200

    solution of the second  example

--- README.md
+++ README.md
@@ -35,14 +35,14 @@ checkDriverAge();
 //2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
 
 var checkDriverAge2 = function() {
-    var age = prompt("What is your age?");
-    if (Number(age) < 18) {
-        alert("Sorry, you are too young to drive this car. Powering off");
-    } else if (Number(age) > 18) {
-        alert("Powering On. Enjoy the ride!");
-    } else if (Number(age) === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
-    }
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
 }
 
 checkDriverAge2();
--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function checkDriverAge() {
-    var age = prompt("What is your age?");
-    if (Number(age) < 18) {
-        alert("Sorry, you are too young to drive this car. Powering off");
-    } else if (Number(age) > 18) {
-        alert("Powering On. Enjoy the ride!");
-    } else if (Number(age) === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
-    }
+// function checkDriverAge() {
+//     var age = prompt("What is your age?");
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too young to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+// checkDriverAge();
+
+
+
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
 }
 
-checkDriverAge();
-
-
-
+checkDriverAge2();
 
 
