e7ddc23ccf86d1d3ce065a5ebac539a9e837bb4c
marcinpodgorski <podgorski.marcin93@gmail.com>
2023-09-01 16:49:08 +0200

    bonus solution

--- script.js
+++ script.js
@@ -13,8 +13,21 @@
 
 
 
-var checkDriverAge2 = function() {
-	var age = prompt("What is your age?");
+// var checkDriverAge2 = function() {
+// 	var age = prompt("What is your age?");
+// 	if (Number(age) < 18) {
+// 		alert("Sorry, you are too yound to drive this car. Powering off");
+// 	} else if (Number(age) > 18) {
+// 		alert("Powering On. Enjoy the ride!");
+// 	} else if (Number(age) === 18) {
+// 		alert("Congratulations on your first year of driving. Enjoy the ride!");
+// 	}
+// }
+
+// checkDriverAge2();
+
+
+function checkDriverAge(age) {
 	if (Number(age) < 18) {
 		alert("Sorry, you are too yound to drive this car. Powering off");
 	} else if (Number(age) > 18) {
@@ -23,7 +36,3 @@ var checkDriverAge2 = function() {
 		alert("Congratulations on your first year of driving. Enjoy the ride!");
 	}
 }
-
-checkDriverAge2();
-
-
