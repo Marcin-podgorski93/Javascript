89b7ccfad9fbcc9181968724525a94f8ded3d2fb
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-01-10 13:30:32 +0100

    exercise JS .filter

--- exercise JS.js
+++ exercise JS.js
@@ -709,3 +709,32 @@ function checkRepeat(word) {
 }
 
 console.log(checkRepeat("marcin"))
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+
+function friend(friends){
+    const friendFilter = friends.filter(word => word.length === 4);
+    return friendFilter;
+  }
+
+
+console.log(friend(["Ryan","Kieran","Mark"]));
+
+//or 
+
+function friend1(friends){
+    return friends.filter(word => word.length == 4);
+  }
+
+
+console.log(friend1(["Ryan","Kieran","Mark"]));
