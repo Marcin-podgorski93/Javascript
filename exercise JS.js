57f1d71d7a5f2ecc56463ab2beb387f611ae20fe
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-02-08 23:14:22 +0100

    exercise

--- exercise JS.js
+++ exercise JS.js
@@ -1018,3 +1018,58 @@ function nbYears(p0, percent, aug, p) {
 }
 
 console.log(nbYears(1500, 5, 100, 5000))
+
+
+// DESCRIPTION:
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+function basicOp(operation, value1, value2) {
+    let result;
+    switch (operation) {
+        case '+':
+            result = value1 + value2;
+            break;
+            case '-':
+                result = value1 - value2;
+                break;
+                case '/':
+                    result = value1 / value2;
+                break;
+                    case '*':
+                result = value1 * value2;
+                break;
+        default:
+            result = 'Unsupported operator';
+    }
+    return result;
+}
+
+console.log(basicOp('+', 4, 4))
+
+// or
+
+function basicOp1(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
+
