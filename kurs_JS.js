a32a876f4e1a394403c2dbe80919d620419d8fa7
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-12-03 22:58:31 +0100

    more exercise

--- kurs_JS.js
+++ kurs_JS.js
@@ -1,6 +1,48 @@
 var saying = "Home sweet home";
-saying.length;
-2 + 2;
 
 console.log(saying.length);
 console.log(saying.indexOf("o"));
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+console.log(doubleInteger(2));
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+var tekst = "aba";
+
+function count(string) {
+  const result = {};
+
+  for (let i = 0; i < string.length; i++) {
+    const element = string[i];
+    console.log(element);
+    if (result[element]) {
+      result[element]++;
+    } else {
+      result[element] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(count(tekst));
+
+function count1(string) {
+  const result = {};
+
+  for (let i = 0; i < string.length; i++) {
+    const element = string[i];
+    console.log(element);
+
+    // uzycie ternary operatora
+    result[element] ? result[element]++ : (result[element] = 1);
+  }
+  return result;
+}
+
+console.log(count1(tekst));
