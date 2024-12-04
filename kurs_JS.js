139b0ab6c8728241d99ca47c202c985868997651
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-12-05 00:06:40 +0100

    update

--- kurs_JS.js
+++ kurs_JS.js
@@ -61,3 +61,24 @@ function checkText(str) {
 }
 
 console.log(checkText("abcde"));
+
+function checkText1(str) {
+  str = str.toLowerCase();
+  let charCount = {};
+  for (const char of str) {
+    console.log(char);
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  let duplicates = 0;
+  for (const key in charCount) {
+    if (charCount[key] > 1) {
+      duplicates++;
+      console.log(duplicates);
+    }
+  }
+
+  return duplicates;
+}
+
+console.log(checkText1("aaccde"));
