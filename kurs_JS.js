42c05fd11becb4e0c9e7cf9f083e1780c3d45088
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-12-09 23:59:05 +0100

    check duplicates

--- kurs_JS.js
+++ kurs_JS.js
@@ -130,3 +130,23 @@ function getCount(str) {
 }
 
 console.log(getCount("apple"));
+
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+
+function duplicateCount(text) {
+  const result = {};
+  for (const znak of text) {
+    if (result[znak]) {
+      result[znak]++;
+      console.log(result[znak]);
+    }
+  }
+  return text;
+}
+
+console.log(duplicateCount("aabbcde"));
