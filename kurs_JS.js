b9f774ee1688b164dbb833facd99f36e0fbfe97a
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-12-13 23:56:53 +0100

    count only true strings

--- kurs_JS.js
+++ kurs_JS.js
@@ -156,3 +156,36 @@ function checkEven(num) {
 }
 
 console.log(checkEven(6));
+
+function countSheeps(sheep) {
+  return sheep.filter((num) => num === true).length;
+}
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
